22b035a896a242061481d1401a4531ee1e9961d9
Raul Dominguez <raul.dominguez@dfki.de>
2015-10-02 14:34:57 +0200

    First test of the library, load Asguard model and plot the resulting TransformGraph.

--- test/test_robot.cpp
+++ test/test_robot.cpp
@@ -1,5 +1,8 @@
 #include <boost/test/unit_test.hpp>
 #include <envire_smurf/Robot.hpp>
+#include <orocos_cpp/ConfigurationHelper.hpp>
+#include <envire_core/graph/TransformGraph.hpp>
+#include <envire_core/graph/GraphViz.hpp>
 
 using namespace envire::smurf;
 
@@ -7,4 +10,15 @@ BOOST_AUTO_TEST_CASE(it_should_not_crash_when_welcome_is_called)
 {
     envire::smurf::Robot robot;
     robot.welcome();
+}
+
+BOOST_AUTO_TEST_CASE(load_asguard_smurf)
+{
+    envire::smurf::Robot robot;
+    std::string path = orocos_cpp::ConfigurationHelper::applyStringVariableInsertions("<%=ENV(AUTOPROJ_CURRENT_ROOT) %>/<%=ENV(ASGUARD4)%>");
+    std::cout << "Path to robot model " << path << std::endl;
+    envire::core::TransformGraph robotGraph;
+    envire::core::GraphViz viz;
+    viz.write(robotGraph, "load_asguard_smurf.dot");
+    //robot.loadFromSmurf();
 }
