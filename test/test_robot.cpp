9287da7d642bba8c5db3b09faa96c67f514b346c
Raul Dominguez <raul.dominguez@dfki.de>
2015-10-12 15:21:02 +0200

    Changes in the old configurator helper class applied.

--- test/test_robot.cpp
+++ test/test_robot.cpp
@@ -1,6 +1,6 @@
 #include <boost/test/unit_test.hpp>
 #include <envire_smurf/Robot.hpp>
-#include <orocos_cpp/ConfigurationHelper.hpp>
+#include <orocos_cpp/YAMLConfiguration.hpp>
 #include <envire_core/graph/TransformGraph.hpp>
 #include <envire_core/graph/GraphViz.hpp>
 #include <envire_core/items/Item.hpp>
@@ -14,7 +14,7 @@ BOOST_AUTO_TEST_CASE(it_should_not_crash_when_welcome_is_called)
 BOOST_AUTO_TEST_CASE(load_asguard_smurf)
 {
     envire::envire_smurf::Robot robot;
-    std::string path = orocos_cpp::ConfigurationHelper::applyStringVariableInsertions("<%=ENV(AUTOPROJ_CURRENT_ROOT) %>/<%=ENV(ASGUARD4)%>");
+    std::string path = orocos_cpp::YAMLConfigParser::applyStringVariableInsertions("<%=ENV(AUTOPROJ_CURRENT_ROOT) %>/<%=ENV(ASGUARD4)%>");
     std::cout << "Path to robot model " << path << std::endl;
     envire::core::TransformGraph transformGraph;
     robot.loadFromSmurf(transformGraph, path);
